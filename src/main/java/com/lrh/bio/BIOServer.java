500c08a8da20840ea9fb59037915c3951bb1ecc9
lrh007 <1873355825@163.com>
2020-07-21 22:22:39 +0800

    '添加bio服务端代码'

---
+++ src/main/java/com/lrh/bio/BIOServer.java
@@ -0,0 +1,66 @@
+package com.lrh.bio;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * bio服务端
+ */
+public class BIOServer {
+
+    public static void main(String[] args) {
+        System.out.println("服务器启动了");
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        try {
+            //创建服务端，监听端口
+            ServerSocket serverSocket = new ServerSocket(6666);
+            while(true){
+                System.out.println("等待连接。。。");
+                final Socket socket = serverSocket.accept();
+                System.out.println("连接上一个客户端。。。");
+                executorService.execute(new Runnable() {
+                    public void run() {
+                        handler(socket);
+                    }
+                });
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 读取数据
+     * @param socket
+     */
+    public static void handler(Socket socket){
+        System.out.println("线程信息，id="+Thread.currentThread().getId()+",name="+Thread.currentThread().getName());
+        byte[] bytes = new byte[1024];
+        try {
+            InputStream inputStream = socket.getInputStream();
+            while(true){
+                System.out.println("wait read。。。");
+                int r = inputStream.read(bytes);
+                if(r != -1){
+                    System.out.println(new String(bytes));
+                }else{
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
