cc83f397206b3623eff72def1b6825061ecc678a
梁瑞豪 <18733558325@163.com>
2020-10-21 17:58:48 +0800

    '添加webp所需jar包'

--- src/main/java/com/lrh/netty/screenremotecontrol/client/Util.java
+++ src/main/java/com/lrh/netty/screenremotecontrol/client/Util.java
@@ -1,20 +1,21 @@
 package com.lrh.netty.screenremotecontrol.client;
 
 import com.lrh.netty.screenremotecontrol.client.bean.ImageData;
+import com.sun.deploy.util.StringUtils;
 import com.sun.image.codec.jpeg.JPEGCodec;
 import com.sun.image.codec.jpeg.JPEGEncodeParam;
 import com.sun.image.codec.jpeg.JPEGImageDecoder;
 import com.sun.image.codec.jpeg.JPEGImageEncoder;
 import net.coobird.thumbnailator.Thumbnails;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.List;
 import java.util.zip.*;
@@ -425,18 +426,43 @@ public class Util {
         ImageIO.write(image,"jpg",new File("C:\\Users\\MACHENIKE\\Desktop\\新建文件夹\\2.jpg"));
 */
         //先把原图片缩小成原来的90%大小，质量为60%
-//        BufferedImage bufferedImage1 = Thumbnails.of(bufferedImage).scale(0.1f).outputQuality(1f).outputFormat("jpg").asBufferedImage();
-//        ImageIO.write(bufferedImage1,"jpg",new File("C:\\Users\\MACHENIKE\\Desktop\\新建文件夹\\2.jpg"));
+//        Robot robot = new Robot();
+//        BufferedImage screenCapture = robot.createScreenCapture(new Rectangle(0, 0, 192, 540));
+//        ImageIO.write(screenCapture,"jpg",new File("C:\\Users\\MACHENIKE\\Desktop\\新建文件夹\\1.jpg"));
+////        BufferedImage bufferedImage1 = Thumbnails.of(screenCapture).scale(0.9f).outputQuality(1f).outputFormat("gif").asBufferedImage();
+//        byte[] bytes = bufferedImageTobytes(screenCapture, 0.3f);
+//        BufferedImage image = ImageIO.read(new ByteArrayInputStream(bytes));
+//        ImageIO.write(image,"jpg",new File("C:\\Users\\MACHENIKE\\Desktop\\新建文件夹\\2.jpg"));
 
-        // 字符串超过一定的长度
-        String str = "OimKBp+DNM9N9Ce1A6yFM2VcySnScSBlCWy0FwXapH3jmL2FpjzXYvuFIvJOsdjKW/9RSAeP/q5u\n" +
+        double scale = 0.8;
+        File file = new File("C:\\Users\\MACHENIKE\\Desktop\\新建文件夹\\1.jpg");
+        BufferedImage bufferedImage;
+            bufferedImage = ImageIO.read(file);
+            int width = bufferedImage.getWidth();
+            int height = bufferedImage.getHeight();
+
+            width = (int)(width * scale);
+            height = (int)(height * scale);
+
+            Image image = bufferedImage.getScaledInstance(width, height,
+                    Image.SCALE_SMOOTH);
+            BufferedImage outputImage = new BufferedImage(width, height,
+                    BufferedImage.TYPE_INT_RGB);
+            Graphics graphics = outputImage.getGraphics();
+            graphics.drawImage(image, 0, 0, null);
+            graphics.dispose();
+
+            ImageIO.write(outputImage, "jpg", new File("C:\\Users\\MACHENIKE\\Desktop\\新建文件夹\\3.jpg"));
+
+            // 字符串超过一定的长度
+       /* String str = "OimKBp+DNM9N9Ce1A6yFM2VcySnScSBlCWy0FwXapH3jmL2FpjzXYvuFIvJOsdjKW/9RSAeP/q5u\n" +
                 "6qyArAfXbnccj+cvYL8vMW8FqFZIMwRpVht3hmJuMdDjNubFhL9VBfapTpkIGpTbiXDvNQ2MBjPI\n" +
                 "aLUIGOM1TCW6ZogcEZEc52DuocUEID9+LJHFdT8wN8A2IKEuZZv1vFiNgmvG5vbo4xoulzh1c5kJ\n" +
                 "ZxVOg+q5yPb6eCth6viCoeuGWp2gmktKww/zVmi1ts5EvmD5TGo3qOpmPudzFC7sR9fTZUa3HEEy\n" +
                 "bSVFoh3sWxZS+KwV";
         System.out.println("原始字符串大小="+str.getBytes("gbk").length);
         byte[] bytes = zipCompress(str);
-        System.out.printf("压缩后字符串大小="+bytes.length);
+        System.out.printf("压缩后字符串大小="+bytes.length);*/
 
 
 //        System.out.println("\n原始的字符串为------->" + str);
@@ -455,6 +481,55 @@ public class Util {
 //        String s = Arrays.asList(list).toString();
 //        System.out.println(s.replaceAll("\\[\\[|\\]\\]",""));
 
+    }
+
+    /**
+     *
+     * 自己设置压缩质量来把图片压缩成byte[]
+     *
+     * @param image
+     *            压缩源图片
+     * @param quality
+     *            压缩质量，在0-1之间，
+     * @return 返回的字节数组
+     */
+    public static byte[] bufferedImageTobytes(BufferedImage image, float quality) {
+        // 如果图片空，返回空
+        /*if (image == null) {
+            return null;
+        } */
+        // 得到指定Format图片的writer
+        Iterator<ImageWriter> iter = ImageIO
+                .getImageWritersByFormatName("jpg");// 得到迭代器
+        ImageWriter writer = (ImageWriter) iter.next(); // 得到writer
+
+        // 得到指定writer的输出参数设置(ImageWriteParam )
+        ImageWriteParam iwp = writer.getDefaultWriteParam();
+        iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT); // 设置可否压缩
+        iwp.setCompressionQuality(quality); // 设置压缩质量参数
+
+        iwp.setProgressiveMode(ImageWriteParam.MODE_DISABLED);
+
+        ColorModel colorModel = image.getColorModel();
+        // 指定压缩时使用的色彩模式
+        iwp.setDestinationType(new javax.imageio.ImageTypeSpecifier(colorModel,
+                colorModel.createCompatibleSampleModel(16, 16)));
+
+        // 开始打包图片，写入byte[]
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(); // 取得内存输出流
+        IIOImage iIamge = new IIOImage(image, null, null);
+        try {
+            // 此处因为ImageWriter中用来接收write信息的output要求必须是ImageOutput
+            // 通过ImageIo中的静态方法，得到byteArrayOutputStream的ImageOutput
+            writer.setOutput(ImageIO
+                    .createImageOutputStream(byteArrayOutputStream));
+            writer.write(null, iIamge, iwp);
+
+        } catch (IOException e) {
+            System.out.println("write errro");
+            e.printStackTrace();
+        }
+        return byteArrayOutputStream.toByteArray();
     }
 
     /** Compress a string to a list of output symbols. */
